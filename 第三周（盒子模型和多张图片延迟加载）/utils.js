fad0398f0f01986e1e9118f1554e4a691461c02e
Tian Xi <tianxiallen@163.com>
2017-01-11 17:17:47 +0800

    top

--- 第三周（盒子模型和多张图片延迟加载）/utils.js
+++ 第三周（盒子模型和多张图片延迟加载）/utils.js
@@ -67,7 +67,7 @@ var uitls = {
 
 
 var utils = (function (){
-    var toArray = function (likeAry){
+    function toArray(likeAry){
 
         try{
             return Array.prototype.slice.call(likeAry,0);
@@ -78,27 +78,27 @@ var utils = (function (){
             }
             return ary;
         }
-    },
-        jsonParse = function (jsonStr){
+    }
+    function jsonParse(jsonStr){
 
             return "JSON" in window ? JSON.parse(jsonStr) : eval('('+jsonStr+')');
-        },
-        getRandom = function (n,m){
+        }
+    function getRandom(n,m){
 
             if(isNaN(n)||isNaN(m)){
                 return Math.random();
             }
             return Math.round(Math.random()*(m-n)+n);
-        },
-        prev = function (ele){
+        }
+    function prev(ele){
 
             var prev = ele.previousSibling; // comment text  element
             while ( prev && prev.nodeType != 1){
                 prev = prev.previousSibling;
             }
             return prev;
-        },
-        next = function (ele){
+        }
+    function next(ele){
 
             if("previousElementSibling" in ele){
                 return ele.previousElementSibling;
@@ -108,8 +108,8 @@ var utils = (function (){
                 next = next.nextSibling;
             }
             return next;
-        },
-        children = function (ele,tagName){
+        }
+    function children(ele,tagName){
             var childs = ele.childNodes;
             var ary = [];
             for(var i = 0; i < childs.length; i++){
@@ -129,7 +129,6 @@ var utils = (function (){
             }
             return ary;
         }
-
     function win(attr,val){
         if(typeof val !== 'undefined'){
             document.documentElement[attr] = val;
@@ -138,11 +137,66 @@ var utils = (function (){
         }
         return document.documentElement[attr] || document.body[attr];
     }
+    function offset(ele){
+        var l = 0,t = 0;
+        l += ele.offsetLeft;
+        t += ele.offsetTop;
+        var par = ele.offsetParent;
+        while (par){
+            if(window.navigator.userAgent.indexOf('MSIE 8') === -1){
+                l += par.clientLeft;
+                t += par.clientTop;
+            }
+            l += par.offsetLeft;
+            t += par.offsetTop;
+            par = par.offsetParent;
+        }
+        return {left : l, top : t};
+    }
+    function getCss(ele,attr){
+        var val = null;
+        if(window.getComputedStyle){
+            val = window.getComputedStyle(ele)[attr];
+        }else{
+            if(attr == 'opacity'){
+                val = ele.currentStyle.filter;
+                var reg = /^alpha\(opacity=(\d+(?:\.\d+)?)\)$/;
+                val = reg.test(val) ? reg.exec(val)[1]/100 : 1;
+            }else{
+                val = ele.currentStyle[attr];
+            }
+        }
+        // -5.5px
+        var reg = /^-?\d+(\.\d+)?(px|pt|em|rem|deg)?$/;
+        return reg.test(val) ? parseFloat(val) : val;
+    }
+    function setCss(ele,attr,val){
+        if(attr == 'opacity'){
+            ele.style.opacity = val;
+            ele.style.filter = 'alpha(opacity='+ val*100 + ')';
+            return;
+        }
+        if(attr == 'float'){
+            ele.style.cssFloat = val;
+            ele.style.styleFloat = val;
+            return;
+        }
+        var reg = /^(width|height|left|top|right|bottom|(margin|padding)(Left|Top|Right|Bottom)?)$/;
+        if(reg.test(attr)){
+            if(!isNaN(val)){
+                val += 'px';
+            }
+        }
+        ele.style[attr] = val;
+    }
 
     return {
+        setCss : setCss,
+        getCss : getCss,
+        offset : offset,
         win : win,
         toArray : toArray,
-        //jsonParse : jsonParse,
+        jsonParse : jsonParse,
         getRandom : getRandom,
         prev : prev,
         next : next,
