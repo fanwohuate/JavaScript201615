e0536da79b6c26afbdfebc7cf074b61566124831
zhouxiaotian <1282347298@qq.com>
2017-02-23 18:45:19 +0800

    ajax

--- 第七周/第三天/server.js
+++ 第七周/第三天/server.js
@@ -1,22 +1,39 @@
-//->导入需要的内置模块
 var http = require('http'),
     url = require('url'),
     fs = require('fs');
-
-//->创建服务监听端口
 var server = http.createServer(function (req, res) {
-    //->解析客户端请求的地址和传递的内容
     var urlObj = url.parse(req.url, true),
         pathname = urlObj.pathname,
         query = urlObj.query;
 
-    //->如果客户端想要的是INDEX.HTML,我们需要获取到页面中的代码,并且返回给客户端
-    if (pathname === '/index.html') {
-        var conFile = fs.readFileSync('./index.html');
-
-        res.end(conFile);//->如果只需要返回一次内容,可以合并在一起来写,返回的同时也告诉客户端返回结束了
+    //->客户端静态资源文件请求的处理
+    var reg = /\.([0-9a-zA-Z]+)/i;
+    if (reg.test(pathname)) {
+        var conFile = null,
+            status = 200;
+        try {
+            conFile = fs.readFileSync('.' + pathname);
+        } catch (e) {
+            conFile = 'not found~';
+            status = 404;
+        }
+        var suffix = reg.exec(pathname)[1].toUpperCase(),
+            suffixMIME = 'text/plain';
+        switch (suffix) {
+            case 'HTML':
+                suffixMIME = 'text/html';
+                break;
+            case 'CSS':
+                suffixMIME = 'text/css';
+                break;
+            case 'JS':
+                suffixMIME = 'text/javascript';
+                break;
+        }
+        res.writeHead(status, {
+            'content-type': suffixMIME + ';charset=utf-8;'
+        });
+        res.end(conFile);
     }
 });
-server.listen(8080, function () {
-    console.log('server is success,listening on 8080 port!');
-});
+server.listen(8080);
